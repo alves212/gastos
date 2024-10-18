a5e16fe7f56ab8e157cc6189810ac22dcbd8d5bd
david <apazareynaldo212390@gmail.com>
2024-10-18 19:10:51 +0200

    service adjust

--- service-worker.js
+++ service-worker.js
@@ -1,61 +1,38 @@
-const CACHE_NAME = 'expenses-v1';
+const CACHE_NAME = 'expenses';
 const urlsToCache = [
   '/index.html',
   '/manifest.json',
   '/script.js',
   '/service-worker.js',
   '/styles.css',
-  '/images/money192.png',
-  '/images/money512.png',
-  '/images/fallback.png',
 ];
 
-// Instala o Service Worker e adiciona ao cache
+// Install the Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-      .catch((error) => {
-        console.error('Falha ao armazenar no cache:', error);
-      }),
-  );
-});
-
-// Intercepta requisições e retorna do cache, se possível
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request)
-          .then((networkResponse) => {
-            // Cache dinâmico para novos recursos
-            return caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, networkResponse.clone());
-              return networkResponse;
-            });
-          })
-          .catch(() => {
-            if (event.request.destination === 'image') {
-              return caches.match('/images/fallback.png');
-            }
-          })
-      );
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     }),
   );
 });
 
-// Ativa o Service Worker e limpa caches antigos
+// Intercept requests and serve cached files
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
+
+// Update the Service Worker when a new version is installed
 self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log(`Cache antigo ${cacheName} removido`);
+          if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName);
           }
         }),
